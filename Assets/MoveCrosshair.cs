2e4b263aa09031496cc1923f4dbc1d0b08939d31
tlehtikallio <tlehtikallio@gmail.com>
2019-03-18 10:36:44 +0200

    Crosshair started, joystick pack added

---
+++ Assets/MoveCrosshair.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCrosshair : MonoBehaviour
+{
+    //public Joystick joystick;
+    float CrosshairMoveSpeed = 1000f;
+    Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 direction = Vector3.up * joystick.Vertical + Vector3.right * joystick.Horizontal; // Suunnat oikein
+        //rb.velocity = direction * CrosshairMoveSpeed * Time.fixedDeltaTime;
+    }
+}
